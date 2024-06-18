009925b01a6726d18ffd448b1a545c97e0ac0f06
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-18 14:16:32 +0300

    onboarding done ✅

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -5,6 +5,10 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login '),
+      ),
+    );
   }
 }
--- lib/features/auth/presentation/views/sign_up.dart
+++ lib/features/auth/presentation/views/sign_up.dart
@@ -5,6 +5,9 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('Sign Up'),
+    ));
   }
 }
