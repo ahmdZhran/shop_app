f17bdcbe6d50b113fe9ebb5eadf2364711a77d9b
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-18 11:26:13 +0300

    reomve comment

--- lib/core/app/shop_app.dart
+++ lib/core/app/shop_app.dart
@@ -5,7 +5,6 @@ import '../../features/onboarding/presentation/views/onboarding_view.dart';
 class ShopApp extends StatelessWidget {
   const ShopApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
