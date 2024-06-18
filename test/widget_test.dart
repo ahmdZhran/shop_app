1fb57974c96017d6de098dbe4d8e20656426267c
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-18 12:43:16 +0300

    create app assets and add first assets that belong to onboarding

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shop_app/core/app/shop_app.dart';
-
+import 'package:shop_app/core/router/app_router.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ShopApp());
+    await tester.pumpWidget(ShopApp(
+      appRouter: AppRouter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
