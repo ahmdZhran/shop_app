79ea6c8e9f7e053b2bcda0ed1fa304a3a4e8475d
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-19 14:58:05 +0300

    calll setupGetIt in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/core/di/dependency_injection.dart';
 
 import 'core/app/shop_app.dart';
 import 'core/router/app_router.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setupGetIt();
   runApp(ShopApp(
     appRouter: AppRouter(),
   ));
