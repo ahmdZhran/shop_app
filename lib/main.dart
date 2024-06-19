6ed16bd446e011ef735cbae891064e5e0e3f15f3
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-19 14:38:21 +0300

    create sharedprefrnece and shared pref keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'core/app/shop_app.dart';
-import 'core/helper/shared_pref.dart';
 import 'core/router/app_router.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SharedPref().instantiatePreferences();
   runApp(ShopApp(
     appRouter: AppRouter(),
   ));
