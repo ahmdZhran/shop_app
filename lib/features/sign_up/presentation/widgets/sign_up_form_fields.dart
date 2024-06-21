972c87c3775ec5f6312d8711804a8bc5855a6bbe
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-21 22:38:07 +0300

    take value from textFormField in ui to textControllers in cubit

--- lib/features/sign_up/presentation/widgets/sign_up_form_fields.dart
+++ lib/features/sign_up/presentation/widgets/sign_up_form_fields.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/features/sign_up/logic/cubit/signup_cubit.dart';
 import '../../../../core/utils/app_strings.dart';
 import '../../../../core/widgets/custom_text_form_fields.dart';
 import '../../../login/presentation/widgets/is_has_an_account.dart';
 
 import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/custom_buttons.dart';
+import '../../logic/cubit/signup_state.dart';
 import 'pick_image_widget.dart';
 
 class SignUpFormFiedls extends StatelessWidget {
@@ -12,78 +15,72 @@ class SignUpFormFiedls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: GlobalKey<FormState>(), // Form key
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            const PickImageWidget(),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              onChanged: (name) {
-                // Handle name change
-              },
-              lableText: AppStrings.fullName,
-              keyboardType: TextInputType.name,
-            ),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              onChanged: (email) {
-                // Handle email change
-              },
-              lableText: AppStrings.emailAdress,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              onChanged: (phone) {
-                // Handle phone change
-              },
-              lableText: AppStrings.phone,
-              keyboardType: TextInputType.phone,
-            ),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              onChanged: (password) {
-                // Handle password change
-              },
-              obscureText: true,
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.visibility_outlined),
+    return BlocListener<SignupCubit, SignupState>(
+      listener: (context, state) {},
+      child: Form(
+        key: context.read<SignupCubit>().formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              const PickImageWidget(),
+              const SizedBox(height: 16),
+              CustomTextFormField(
+                controller: context.read<SignupCubit>().fullnameController,
+                lableText: AppStrings.fullName,
+                keyboardType: TextInputType.name,
+              ),
+              const SizedBox(height: 16),
+              CustomTextFormField(
+                controller: context.read<SignupCubit>().emailController,
+                lableText: AppStrings.emailAdress,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 16),
+              CustomTextFormField(
+                controller: context.read<SignupCubit>().phoneController,
+                lableText: AppStrings.phone,
+                keyboardType: TextInputType.phone,
+              ),
+              const SizedBox(height: 16),
+              CustomTextFormField(
+                controller: context.read<SignupCubit>().passwordController,
+                obscureText: true,
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.visibility_outlined),
+                  onPressed: () {
+                    // Handle password visibility toggle
+                  },
+                ),
+                lableText: AppStrings.password,
+              ),
+              const SizedBox(height: 20),
+              CustomButton(
                 onPressed: () {
-                  // Handle password visibility toggle
+                  // Handle sign up button press
                 },
+                text: Text(
+                  AppStrings.signUp,
+                  style: CustomTextStyle.semiBold16,
+                ),
               ),
-              lableText: AppStrings.password,
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              onPressed: () {
-                // Handle sign up button press
-              },
-              text: Text(
-                AppStrings.signUp,
-                style: CustomTextStyle.semiBold16,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const IsHasAnAccount(
-                // textOne: AppStrings.alreadyHaveAccount,
-                // textTwo: AppStrings.singIn,
-                // onTap: () {
-                //   Navigator.push(context, MaterialPageRoute(
-                //     builder: (context) {
-                //       return const LoginView();
-                //     },
-                //   ));
-                // },
-                )
-          ],
+              const SizedBox(height: 20),
+              const IsHasAnAccount(
+                  // textOne: AppStrings.alreadyHaveAccount,
+                  // textTwo: AppStrings.singIn,
+                  // onTap: () {
+                  //   Navigator.push(context, MaterialPageRoute(
+                  //     builder: (context) {
+                  //       return const LoginView();
+                  //     },
+                  //   ));
+                  // },
+                  )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
