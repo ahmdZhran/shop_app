ccb25295540710dea88191b6aafa124db888cc08
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-21 20:52:00 +0300

    add text controllers in cubit

--- lib/features/sign_up/logic/cubit/signup_cubit.dart
+++ lib/features/sign_up/logic/cubit/signup_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -5,4 +6,9 @@ part 'signup_state.dart';
 
 class SignupCubit extends Cubit<SignupState> {
   SignupCubit() : super(const SignupState.initial());
+  
+  TextEditingController fullnameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 }
