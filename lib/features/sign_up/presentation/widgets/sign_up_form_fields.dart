6410a87a502b3220f007f273af56ca3a04f07d4f
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-22 19:46:29 +0300

    listen to states in ui

--- lib/features/sign_up/presentation/widgets/sign_up_form_fields.dart
+++ lib/features/sign_up/presentation/widgets/sign_up_form_fields.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/core/helper/extensions.dart';
 import 'package:shop_app/core/widgets/already_have_an_account.dart';
 import 'package:shop_app/features/sign_up/logic/cubit/signup_cubit.dart';
+import '../../../../core/router/routes.dart';
 import '../../../../core/utils/app_strings.dart';
+import '../../../../core/utils/color_manager.dart';
 import '../../../../core/widgets/custom_text_form_fields.dart';
 import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/custom_buttons.dart';
@@ -29,7 +32,31 @@ class _SignUpFormFiedlsState extends State<SignUpFormFiedls> {
   @override
   Widget build(BuildContext context) {
     return BlocListener<SignupCubit, SignupState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        state.whenOrNull(
+          signuploading: () {
+            showDialog(
+              context: context,
+              builder: (context) => Center(
+                child: CircularProgressIndicator(
+                  color: ColorManager.kPrimaryColor,
+                ),
+              ),
+            );
+          },
+          signupSuccess: (signUpResponse) {
+            context.pop();
+            context.pushNamed(Routes.login);
+          },
+          error: (error) {
+            context.pop();
+            ScaffoldMessenger.maybeOf(context)!.showSnackBar(SnackBar(
+              content: Text(error),
+              backgroundColor: Colors.red,
+            ));
+          },
+        );
+      },
       child: Form(
         key: context.read<SignupCubit>().formKey,
         child: Padding(
@@ -92,8 +119,9 @@ class _SignUpFormFiedlsState extends State<SignUpFormFiedls> {
       ),
     );
   }
-  void validateAndSignup(){
-    if(context.read<SignupCubit>().formKey.currentState!.validate()){
+
+  void validateAndSignup() {
+    if (context.read<SignupCubit>().formKey.currentState!.validate()) {
       context.read<SignupCubit>().emitSignupStates();
     }
   }
