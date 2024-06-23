99f889d8497f05eaf40cb0fddf7822efa5837535
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-23 14:25:58 +0300

    create list of card item

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shop_app/core/helper/extensions.dart';
 import '../widgets/cart_head_icon.dart';
 import '../widgets/custom_search_bar.dart';
+import '../widgets/custom_staggered_gride_view.dart';
 import '../widgets/electronic_details.dart';
 import '../widgets/flash_sales_container.dart';
 import '../widgets/list_of_category.dart';
@@ -23,11 +26,11 @@ class HomeView extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const CartHeadIcon(),
-                const SizedBox(height: 16),
+                10.0.getVerticalSpacer(),
                 const CustomSearchBar(),
-                SizedBox(height: screenSize.height * 0.05),
+                15.0.getVerticalSpacer(),
                 const FlashSalesContainer(),
-                SizedBox(height: screenSize.height * 0.03),
+                3.0.getVerticalSpacer(),
                 const Row(
                   children: [
                     CategoryTextWidget(),
@@ -35,8 +38,10 @@ class HomeView extends StatelessWidget {
                     SeeAllTextWidget(),
                   ],
                 ),
-                SizedBox(height: screenSize.height * 0.02),
-                const ListOfCategory()
+                2.0.getVerticalSpacer(),
+                const ListOfCategory(),
+                10.0.getVerticalSpacer(),
+                const CustomStaggeredGridView()
               ],
             ),
           ),
