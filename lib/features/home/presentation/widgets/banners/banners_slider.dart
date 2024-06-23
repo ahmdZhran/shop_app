9b78e9b01453a6590d999095bd713ea1620ba3eb
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-24 01:57:10 +0300

    create list to bannerslider

--- lib/features/home/presentation/widgets/banners/banners_slider.dart
+++ lib/features/home/presentation/widgets/banners/banners_slider.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../../../core/utils/color_manager.dart';
+import 'banner_contaner.dart';
 
 class BannersSlider extends StatelessWidget {
   const BannersSlider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 5,
-      ),
-      child: Container(
-          width: double.infinity,
-          height: 100.h,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: ColorManager.kPrimaryColor,
-          ),
-          child: Image.asset(
-            'assets/images/backaground.webp',
-            fit: BoxFit.fill,
-          )),
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return const BannerContainer();
+      },
     );
   }
 }
