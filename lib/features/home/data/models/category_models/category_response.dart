2878aadf657c774ed8c6c6b5171c76e59f7d6309
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-25 19:54:23 +0300

    remove deprecated code

--- lib/features/home/data/models/category_models/category_response.dart
+++ lib/features/home/data/models/category_models/category_response.dart
@@ -1,37 +1,4 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
-// part 'category_response.g.dart';
-
-// @JsonSerializable()
-// class CategoryResponse {
-//   bool? status;
-//   String? message;
-//   @JsonKey(name: 'data')
-//   List<CategoryData>? categoryData;
-//   CategoryResponse({
-//     this.status,
-//     this.message,
-//     this.categoryData,
-//   });
-
-//   factory CategoryResponse.fromJson(Map<String, dynamic> json) =>
-//       _$CategoryResponseFromJson(json);
-// }
-
-// @JsonSerializable()
-// class CategoryData {
-//   int? id;
-//   String? name;
-//   String? image;
-//   CategoryData({
-//     this.id,
-//     this.name,
-//     this.image,
-//   });
-//   factory CategoryData.fromJson(Map<String, dynamic> json) =>
-//       _$CategoryDataFromJson(json);
-// }
 import 'package:freezed_annotation/freezed_annotation.dart';
-
 part 'category_response.g.dart';
 
 @JsonSerializable()
