f8a91cc67c62f51f44fd3c09971f755fac7cdb06
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-25 15:43:42 +0300

    do a generate to homApiService for changes that we did in this class

--- lib/features/home/data/api/home_api_service.g.dart
+++ lib/features/home/data/api/home_api_service.g.dart
@@ -47,6 +47,33 @@ class _HomeApiService implements HomeApiService {
     return value;
   }
 
+  @override
+  Future<BannerResponse> getCategories() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<BannerResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/categories',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = BannerResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
