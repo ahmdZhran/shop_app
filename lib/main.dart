6045fb86d5d01c525d9a3504a4a947cc890d3ffa
Ahmed <ahmedmohameddx80@gmail.com>
2024-06-26 16:26:26 +0300

    create extension on string

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/core/helper/extensions.dart';
+import 'package:shop_app/core/helper/shared_prefrence.dart';
 import 'core/di/dependency_injection.dart';
 
 import 'core/app/shop_app.dart';
+import 'core/helper/shared_prefrence_keys.dart';
 import 'core/router/app_router.dart';
-
+bool isLoggedInUser = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   runApp(ShopApp(appRouter: AppRouter()));
+}
+checkLoggedInUser()async {
+  String? userToken = await SharedPrefHelper.getString(SharedPrefKeys.userToken);
+  if(!userToken.isNullOrEmpty()){
+    isLoggedInUser = true;
+  }else{
+    isLoggedInUser = false;
+  }
 }
