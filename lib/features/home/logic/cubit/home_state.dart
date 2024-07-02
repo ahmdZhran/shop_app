bd618e3054aa3b3ea2b5c4a3fbe7903e47f514d6
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-02 20:29:21 +0300

    remove unused code of an old state

--- lib/features/home/logic/cubit/home_state.dart
+++ lib/features/home/logic/cubit/home_state.dart
@@ -7,12 +7,6 @@ part 'home_state.freezed.dart';
 class HomeState with _$HomeState {
   const factory HomeState.initial() = _Initial;
 
-  // banner states
-  // const factory HomeState.bannerLoading() = BannerLoading;
-  // const factory HomeState.bannerSuccess(BannerResponse bannerResponse) =
-  //     BannerSuccess;
-  // const factory HomeState.bannerError({required String error}) = BannerError;
-
   // category states
   const factory HomeState.categoryLoading() = CategoryLoading;
   const factory HomeState.categorySuccess(CategoryResponse categoryResponse) =
