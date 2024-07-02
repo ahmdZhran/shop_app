b043e69834443cda809456673d4741f79c9c7451
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-02 08:09:19 +0300

    create enum for nav bar

--- lib/features/main/logic/cubit/nav_bar_cubit.dart
+++ lib/features/main/logic/cubit/nav_bar_cubit.dart
@@ -2,8 +2,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'nav_bar_state.dart';
 
-
-
 class NavBarCubit extends Cubit<NavBarState> {
   NavBarCubit() : super(const NavBarState.initial());
 }
---
+++ lib/features/main/logic/enums/nav_bar_enum.dart
@@ -0,0 +1,6 @@
+enum NavBarEnum{
+home,
+search,
+favorites,
+profile
+}
