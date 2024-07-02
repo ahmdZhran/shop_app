5d1836122db61f02731373e3c711a90be143f7ea
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-02 08:03:33 +0300

    create cubit and add its states

--- lib/features/main/logic/cubit/nav_bar_cubit.dart
+++ lib/features/main/logic/cubit/nav_bar_cubit.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'nav_bar_state.dart';
+import 'nav_bar_state.dart';
+
+
 
 class NavBarCubit extends Cubit<NavBarState> {
   NavBarCubit() : super(const NavBarState.initial());
--- lib/features/main/logic/cubit/nav_bar_state.dart
+++ lib/features/main/logic/cubit/nav_bar_state.dart
@@ -1,6 +1,10 @@
-part of 'nav_bar_cubit.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'nav_bar_state.freezed.dart';
 
 @freezed
 class NavBarState with _$NavBarState {
   const factory NavBarState.initial() = _Initial;
+
+  const factory NavBarState.iconSelected() = IconSelected;
 }
