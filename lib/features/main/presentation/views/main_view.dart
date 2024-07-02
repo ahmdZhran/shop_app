b805617909a5441cbbb66553798c6cf6b4f2b4d2
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-02 07:09:10 +0300

    create the body of bottom nav bar

--- lib/features/main/presentation/views/main_view.dart
+++ lib/features/main/presentation/views/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/features/main/widgets/bottom_nav_bar/main_nav_bar.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -11,6 +12,7 @@ class MainView extends StatelessWidget {
       child: const Column(
         children: [
           Expanded(child: Text('Our Screens')),
+          MainBottonNavBar(),
         ],
       ),
     ));
---
+++ lib/features/main/widgets/bottom_nav_bar/main_nav_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shop_app/core/animations/animate_do.dart';
+import 'package:shop_app/core/utils/color_manager.dart';
+
+class MainBottonNavBar extends StatelessWidget {
+  const MainBottonNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInUp(
+        duration: 800,
+        child: SizedBox(
+            height: 50.h,
+            width: double.infinity,
+            child: Stack(
+              children: [
+                const SizedBox(height: 15),
+                Container(
+                  height: 60.h,
+                  decoration: BoxDecoration(
+                      color: ColorManager.kPrimaryColor,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60),
+                      )),
+                )
+              ],
+            )));
+  }
+}
