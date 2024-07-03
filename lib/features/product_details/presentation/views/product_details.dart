fb6b7c8b98b73d3cd27043e848ca6409df8778ed
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-03 17:19:31 +0300

    convert to stateless widget

--- lib/features/product_details/presentation/views/product_details.dart
+++ lib/features/product_details/presentation/views/product_details.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
 import '../../../../core/helper/extensions.dart';
 import '../../../onboarding/presentation/widgets/custom_smooth_page_indicator.dart';
-import '../widgets/custom_app_bar.dart';
 import 'images_page_view_builder.dart';
 
-class ProductDetailsView extends StatefulWidget {
-  const ProductDetailsView(
-      {super.key, required this.productId, required this.images});
+class ProductDetailsView extends StatelessWidget {
+  const ProductDetailsView({
+    super.key,
+    required this.productId,
+    required this.images,
+  });
+
   final int productId;
   final List<String> images;
 
   @override
-  State<ProductDetailsView> createState() => _ProductDetailsViewState();
-}
-
-class _ProductDetailsViewState extends State<ProductDetailsView> {
-  final PageController _controller = PageController();
-
-  @override
   Widget build(BuildContext context) {
+    final PageController controller = PageController();
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const CustomAppBarWidget(),
-          ImagesPageViewBuilder(controller: _controller, widget: widget),
-          10.0.getVerticalSpacer(),
-          CustomSmoothPageIndicator(
-              countOfDots: widget.images.length,
-              controller: _controller,
-              widthOfDot: 10,
-              heighOfDot: 10,
-              widthOfBackGroundDot: 15,
-              heightOfBackGroundDot: 15),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 300.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: ImagesPageViewBuilder(
+                controller: controller,
+                widget: this,
+              ),
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(50.0),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: CustomSmoothPageIndicator(
+                  countOfDots: images.length,
+                  controller: controller,
+                  widthOfDot: 10,
+                  heighOfDot: 10,
+                  widthOfBackGroundDot: 15,
+                  heightOfBackGroundDot: 15,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                10.0.getVerticalSpacer(),
+                // Add other content here
+              ],
+            ),
+          ),
         ],
       ),
     );
