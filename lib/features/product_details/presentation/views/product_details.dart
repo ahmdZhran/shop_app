02a53b5b1313d9341c4cb6d70e8ec485147ac9ee
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-03 16:48:54 +0300

    refactor prodcuct details view

--- lib/features/product_details/presentation/views/product_details.dart
+++ lib/features/product_details/presentation/views/product_details.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shop_app/core/helper/extensions.dart';
+import 'package:shop_app/core/utils/color_manager.dart';
 import 'package:shop_app/features/onboarding/presentation/widgets/custom_smooth_page_indicator.dart';
 import '../widgets/custom_app_bar.dart';
+import 'images_page_view_builder.dart';
 
 class ProductDetailsView extends StatefulWidget {
   const ProductDetailsView(
@@ -24,34 +27,15 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
       body: Column(
         children: [
           const CustomAppBarWidget(),
-          SizedBox(
-            height: 300.h,
-            width: double.infinity,
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: widget.images.length,
-              itemBuilder: (context, index) {
-                return Hero(
-                  tag: widget.productId,
-                  child: CachedNetworkImage(
-                    imageUrl: widget.images[index],
-                    fit: BoxFit.contain,
-                    placeholder: (context, url) =>
-                        const Center(child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) =>
-                        const Center(child: Icon(Icons.error)),
-                  ),
-                );
-              },
-            ),
-          ),
+          ImagesPageViewBuilder(controller: _controller, widget: widget),
+          10.0.getVerticalSpacer(),
           CustomSmoothPageIndicator(
               countOfDots: widget.images.length,
               controller: _controller,
               widthOfDot: 10,
               heighOfDot: 10,
               widthOfBackGroundDot: 15,
-              heightOfBackGroundDot: 15)
+              heightOfBackGroundDot: 15),
         ],
       ),
     );
