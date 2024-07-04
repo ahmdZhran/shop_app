3980e5e9597f708bf567a5036932b537b0cfe74e
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-04 14:27:52 +0300

    create first method to can fetch all itemes that has been added to favorit

--- lib/features/favorits/data/cubit/favorit_cubit.dart
+++ lib/features/favorits/data/cubit/favorit_cubit.dart
@@ -1,6 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop_app/features/favorits/data/models/favorite_item_model.dart';
-
 import '../repo/favorite_repo.dart';
 import 'favorit_state.dart';
 
@@ -9,8 +7,17 @@ class FavoritCubit extends Cubit<FavoritState> {
 
   final FavoriteRepo _favoritRepo;
 
-  void addToFavorite(FavoriteItemModel item) {
-    _favoritRepo.addToFavorite(item);
-    emit(FavoritState.favoriteAdded(_favoritRepo.getFavoriteItems()));
+  void fetchFavoriteItems() {
+    emit(const FavoritState.loading());
+    try {
+      final favoriteItems = _favoritRepo.getFavoriteItems();
+      if (favoriteItems.isEmpty) {
+        emit(const FavoritState.favoriteEmpty(message: 'empty'));
+      } else {
+        emit(FavoritState.favoriteAdded(favoriteItems));
+      }
+    } catch (e) {
+      emit(FavoritState.favoriteError(message: e.toString()));
+    }
   }
 }
