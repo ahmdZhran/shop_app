ec178d3df26b50ddc58783c1f5710a5ba4fde14f
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-04 14:32:07 +0300

    create add to favorit method

--- lib/features/favorits/data/cubit/favorit_cubit.dart
+++ lib/features/favorits/data/cubit/favorit_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../models/favorite_item_model.dart';
 import '../repo/favorite_repo.dart';
 import 'favorit_state.dart';
 
@@ -7,7 +8,7 @@ class FavoritCubit extends Cubit<FavoritState> {
 
   final FavoriteRepo _favoritRepo;
 
-  void fetchFavoriteItems() {
+  void fetchFavoritItems() {
     emit(const FavoritState.loading());
     try {
       final favoriteItems = _favoritRepo.getFavoriteItems();
@@ -16,8 +17,19 @@ class FavoritCubit extends Cubit<FavoritState> {
       } else {
         emit(FavoritState.favoriteAdded(favoriteItems));
       }
-    } catch (e) {
-      emit(FavoritState.favoriteError(message: e.toString()));
+    } catch (error) {
+      emit(FavoritState.favoriteError(message: error.toString()));
     }
   }
+
+  void addToFavorit(FavoriteItemModel item) {
+    try {
+      _favoritRepo.addToFavorite(item);
+      fetchFavoritItems();
+    } catch (error) {
+      emit(FavoritState.favoriteError(message: error.toString()));
+    }
+  }
+
+  
 }
