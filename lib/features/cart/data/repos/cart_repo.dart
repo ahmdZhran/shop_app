46d27fa2b53de000fab6af53743258706a80886a
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-05 16:43:50 +0300

    create cart repo

---
+++ lib/features/cart/data/repos/cart_repo.dart
@@ -0,0 +1,19 @@
+import 'package:shop_app/core/networking/api_error_handler.dart';
+import 'package:shop_app/core/networking/api_result.dart';
+import 'package:shop_app/features/cart/data/api/cart_api_service.dart';
+
+import '../models/cart_response.dart';
+
+class CartRepo{
+ final CartApiService _cartApiService;
+ CartRepo(this._cartApiService);
+
+ Future<ApiResult<CartResponse>> getCart() async {
+   try {
+     final response = await _cartApiService.getCart();
+     return ApiResult.success(response);
+   } catch (error) {
+     return ApiResult.failure(ErrorHandler.handle(error));
+   }
+ } 
+}
