9ec0e8f1b5453ba2a53819c14e22eb3bb9bff037
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-05 16:29:31 +0300

    add rest api with base url in api service

--- lib/features/cart/data/api/cart_api_service.dart
+++ lib/features/cart/data/api/cart_api_service.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
 
+import '../../../../core/networking/api_constants.dart';
 
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
 class CartApiService{
   factory CartApiService(Dio dio, {String baseUrl}) = _CartApiService;
 
