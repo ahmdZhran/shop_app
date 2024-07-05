ae802d8cb2c5aaba1442f026cc6979eed98638e2
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-05 09:00:05 +0300

    favorite view is done ✅💯

--- lib/features/home/presentation/widgets/products_section/product_card_header.dart
+++ lib/features/home/presentation/widgets/products_section/product_card_header.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop_app/features/favorits/data/models/favorite_item_model.dart';
-
-import '../../../../../core/utils/color_manager.dart';
-import '../../../../favorits/cubit/favorit_cubit.dart';
-import '../../../../favorits/cubit/favorit_state.dart';
+import '../../../../favorits/presentations/widgets/favorite_button_bloc_builder.dart';
 
 class ProductCardHeader extends StatelessWidget {
   const ProductCardHeader({
@@ -21,7 +15,6 @@ class ProductCardHeader extends StatelessWidget {
   });
 
   final String discount;
-  final bool isFavorite = false;
   final String description;
   final String oldPrice;
   final int productId;
@@ -29,6 +22,7 @@ class ProductCardHeader extends StatelessWidget {
   final String imageurl;
   final String price;
   final List<String> images;
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -53,33 +47,16 @@ class ProductCardHeader extends StatelessWidget {
             ),
           ),
         const Spacer(),
-        BlocBuilder<FavoritCubit, FavoritState>(
-          builder: (context, state) {
-            final isFavorite =
-                context.read<FavoritCubit>().isFavorite(productId);
-            return IconButton(
-              onPressed: () {
-                final favoriteItem = FavoriteItemModel(
-                  id: productId,
-                  name: titleOfItem,
-                  image: imageurl,
-                  price: price,
-                  images: images,
-                  discount: discount,
-                  oldPrice: oldPrice,
-                  description: description,
-                );
-                if (isFavorite) {
-                  context.read<FavoritCubit>().removeFromFavorit(productId);
-                } else {
-                  context.read<FavoritCubit>().addToFavorit(favoriteItem);
-                }
-              },
-              icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
-              color: ColorManager.kPrimaryColor,
-            );
-          },
-        ).animate(target: isFavorite ? 1 : 0).scaleXY(end: 3.2),
+        FavoritButtonBlocBuilder(
+          productId: productId,
+          titleOfItem: titleOfItem,
+          imageurl: imageurl,
+          price: price,
+          images: images,
+          discount: discount,
+          oldPrice: oldPrice,
+          description: description,
+        ),
       ],
     );
   }
