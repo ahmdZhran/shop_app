ddba88e35cacdb8c238bc205b547ea8e85dec765
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-05 17:41:10 +0300

    create cart cubit

--- lib/features/cart/cubits/cubit/cart_cubit.dart
+++ lib/features/cart/cubits/cubit/cart_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/features/cart/data/repos/cart_repo.dart';
 import 'cart_state.dart';
 
-
 class CartCubit extends Cubit<CartState> {
-  CartCubit() : super(const CartState.initial());
+  CartCubit(this._cartRepo) : super(const CartState.initial());
+  final CartRepo _cartRepo;
+  void fetchCart() async {
+    try {
+      emit(const CartState.cartLoading());
+      final response = await _cartRepo.getCart();
+      response.when(success: (cartResponse) {
+        emit(CartState.cartSuccess(cartResponse));
+      }, failure: (error) {
+        emit(CartState.cartError(error: error.apiErrorModel.message ?? ""));
+      });
+    } catch (error) {
+      emit(CartState.cartError(error: error.toString()));
+    }
+  }
 }
