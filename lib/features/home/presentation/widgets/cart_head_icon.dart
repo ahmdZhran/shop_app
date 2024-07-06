8b5108182557257d2b1053197b496a07aad49b71
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-06 14:53:24 +0300

    provide the cart cubit on the icone

--- lib/features/home/presentation/widgets/cart_head_icon.dart
+++ lib/features/home/presentation/widgets/cart_head_icon.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:shop_app/core/di/dependency_injection.dart';
 import 'package:shop_app/core/helper/extensions.dart';
 import '../../../../core/router/routes.dart';
 import '../../../../core/utils/color_manager.dart';
 import '../../../../core/utils/app_assets.dart';
+import '../../../cart/cubits/cubit/cart_cubit.dart';
 
 class CartHeadIcon extends StatelessWidget {
   const CartHeadIcon({super.key});
@@ -12,15 +15,18 @@ class CartHeadIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topRight,
-      child: IconButton(
-        icon: SvgPicture.asset(
-          AppAssets.cart,
-          colorFilter:
-              ColorFilter.mode(ColorManager.kPrimaryColor, BlendMode.srcIn),
+      child: BlocProvider(
+        create: (context) => CartCubit(getIt()),
+        child: IconButton(
+          icon: SvgPicture.asset(
+            AppAssets.cart,
+            colorFilter:
+                ColorFilter.mode(ColorManager.kPrimaryColor, BlendMode.srcIn),
+          ),
+          onPressed: () {
+            context.pushNamed(Routes.cart);
+          },
         ),
-        onPressed: () {
-          context.pushNamed(Routes.cart);
-        },
       ),
     );
   }
