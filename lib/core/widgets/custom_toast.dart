b70adfb2cc62165e706875862463648b4531fefd
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-13 19:23:46 +0300

    remove unused import file

--- lib/core/widgets/custom_toast.dart
+++ lib/core/widgets/custom_toast.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:toast/toast.dart';
 
 class ShowToast {
   const ShowToast._();
