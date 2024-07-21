626bd37efb7cb6701c7ae01edab31aa429ed5adb
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-21 16:01:50 +0300

    display all total of all items and it increase and descrease according the sum of our items

--- lib/features/cart/presentation/views/cart_view.dart
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/core/utils/color_manager.dart';
 import 'package:shop_app/core/widgets/custom_toast.dart';
 import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/custom_buttons.dart';
@@ -7,9 +8,11 @@ import '../../cubits/cubit/cart_cubit.dart';
 import '../../cubits/cubit/cart_state.dart';
 import '../widgets/cart_app_bar_.dart';
 import '../widgets/cart_item_card.dart';
+import '../widgets/cart_total_price.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,54 +23,54 @@ class CartView extends StatelessWidget {
             child: BlocBuilder<CartCubit, CartState>(
               builder: (context, state) {
                 return state.maybeWhen(
-                  cartSuccess: (items ,totalPrice) {
-                    return ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final item = items[index];
-                        return Dismissible(
-                          background: Container(
-                            color: Colors.redAccent,
-                            alignment: Alignment.centerRight,
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: const Icon(
-                              Icons.delete,
-                              size: 30,
-                              color: Colors.white,
-                            ),
+                  cartSuccess: (items, totalPrice) {
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: items.length,
+                            itemBuilder: (context, index) {
+                              final item = items[index];
+                              return Dismissible(
+                                background: Container(
+                                  color: Colors.redAccent,
+                                  alignment: Alignment.centerRight,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: const Icon(
+                                    Icons.delete,
+                                    size: 30,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onDismissed: (direction) {
+                                  context
+                                      .read<CartCubit>()
+                                      .deleteItemFromCart(item.id);
+                                  ShowToast.showToastErrorBottom(
+                                      message: 'Item Deleted From Cart');
+                                },
+                                key: Key(item.id.toString()),
+                                child: CartItemCard(
+                                  imageUrl: item.image,
+                                  itemName: item.name,
+                                  itemPrice: double.parse(item.price),
+                                  itemCount: item.quantity.toInt(),
+                                  itemId: item.id,
+                                ),
+                              );
+                            },
                           ),
-                          onDismissed: (direction) {
-                            context
-                                .read<CartCubit>()
-                                .deleteItemFromCart(item.id);
-                            ShowToast.showToastErrorBottom(
-                                message: 'Item Deleted From Cart');
-                          },
-                          key: Key(item.id.toString()),
-                          child: CartItemCard(
-                            imageUrl: item.image,
-                            itemName: item.name,
-                            itemPrice: double.parse(item.price),
-                            itemCount: item.quantity.toInt(),
-                            itemId: item.id,
-                          ),
-                        );
-                      },
+                        ),
+                        CartTotolPrice(
+                          totalPrice: totalPrice,
+                        ),
+                      ],
                     );
                   },
                   orElse: () => const SizedBox.shrink(),
                 );
               },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CustomButton(
-              onPressed: () {},
-              text: Text(
-                'Check Out',
-                style: CustomTextStyle.medium16,
-              ),
             ),
           ),
         ],
---
+++ lib/features/cart/presentation/widgets/cart_total_price.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/color_manager.dart';
+import '../../../../core/utils/text_styles.dart';
+import '../../../../core/widgets/custom_buttons.dart';
+
+class CartTotolPrice extends StatelessWidget {
+  const CartTotolPrice({
+    super.key,
+    required this.totalPrice,
+  });
+  final double totalPrice;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Total: \$${totalPrice.toStringAsFixed(1)}',
+            style: CustomTextStyle.medium16
+                .copyWith(color: ColorManager.kPrimaryColor),
+          ),
+          const SizedBox(height: 10.0),
+          CustomButton(
+            onPressed: () {},
+            text: Text(
+              'Check Out',
+              style: CustomTextStyle.medium16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
