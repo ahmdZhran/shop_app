f57b3cac26f20502798f2da972039a9d82ba9342
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-21 20:20:41 +0300

    create checkout view

---
+++ lib/features/check_out/presentation/views/check_out.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CheckOutView extends StatelessWidget {
+  const CheckOutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('this is hte checkout'),
+      ),
+    );
+  }
+}
