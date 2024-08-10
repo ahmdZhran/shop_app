98cb358ec3b633bbc586e581e07701cc3c5c6677
Ahmed <ahmedmohameddx80@gmail.com>
2024-08-10 22:55:21 +0300

    create payment methods list view

--- lib/features/check_out/presentation/widgets/checkout_details.dart
+++ lib/features/check_out/presentation/widgets/checkout_details.dart
@@ -7,6 +7,7 @@ import 'package:shop_app/core/widgets/custom_buttons.dart';
 
 import '../../../../core/utils/color_manager.dart';
 import 'detials_of_amount.dart';
+import 'payment_method_bottom_sheet_widget.dart';
 
 class CheckoutDetails extends StatelessWidget {
   const CheckoutDetails({super.key, required this.totalPrice});
@@ -61,45 +62,6 @@ class CheckoutDetails extends StatelessWidget {
 }
 
 
-class PaymentMethodsListView extends StatefulWidget {
-  const PaymentMethodsListView({super.key});
-
-  @override
-  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
-}
-
-class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
-  final List<String> paymentMethodsItems = const [
-    'assets/svg/card.svg',
-    'assets/svg/paypal.svg'
-  ];
-
-  int activeIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 62,
-      child: ListView.builder(
-          itemCount: paymentMethodsItems.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: GestureDetector(
-                onTap: () {
-                  activeIndex = index;
-                  setState(() {});
-                },
-                child: PaymentMethodItem(
-                  isActive: activeIndex == index,
-                  image: paymentMethodsItems[index],
-                ),
-              ),
-            );
-          }),
-    );
-  }
-}
 
 class PaymentMethodItem extends StatelessWidget {
   const PaymentMethodItem({
--- lib/features/check_out/presentation/widgets/payment_method_bottom_sheet_widget.dart
+++ lib/features/check_out/presentation/widgets/payment_method_bottom_sheet_widget.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/custom_buttons.dart';
-import 'checkout_details.dart';
+import 'payment_methods_list.dart';
 
 class PaymentMethodsBottomSheet extends StatelessWidget {
   const PaymentMethodsBottomSheet({super.key});
