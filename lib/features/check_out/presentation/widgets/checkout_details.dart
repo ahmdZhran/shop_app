4db59b5f7e6209d01cc22c3a206a6df98a8f2ab3
Ahmed <ahmedmohameddx80@gmail.com>
2024-08-10 22:53:51 +0300

    create payment method bottom sheet widget

--- lib/features/check_out/presentation/widgets/checkout_details.dart
+++ lib/features/check_out/presentation/widgets/checkout_details.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:shop_app/core/helper/extensions.dart';
 import 'package:shop_app/core/utils/text_styles.dart';
 import 'package:shop_app/core/widgets/custom_buttons.dart';
 
+import '../../../../core/utils/color_manager.dart';
 import 'detials_of_amount.dart';
 
 class CheckoutDetails extends StatelessWidget {
@@ -36,14 +39,109 @@ class CheckoutDetails extends StatelessWidget {
           ),
           100.0.getVerticalSpacer(),
           CustomButton(
-              onPressed: () {
-                
-              },
-              text: Text(
-                'Submet Order',
-                style: CustomTextStyle.medium16,
-              ))
+            onPressed: () {
+              showBottomSheet(
+                  backgroundColor: ColorManager.kBlackColor,
+                  showDragHandle: true,
+                  enableDrag: true,
+                  context: context,
+                  builder: (context) {
+                    return const PaymentMethodsBottomSheet();
+                  });
+            },
+            text: Text(
+              'Submet Order',
+              style: CustomTextStyle.medium16,
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+
+class PaymentMethodsListView extends StatefulWidget {
+  const PaymentMethodsListView({super.key});
+
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
+  final List<String> paymentMethodsItems = const [
+    'assets/svg/card.svg',
+    'assets/svg/paypal.svg'
+  ];
+
+  int activeIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      child: ListView.builder(
+          itemCount: paymentMethodsItems.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GestureDetector(
+                onTap: () {
+                  activeIndex = index;
+                  setState(() {});
+                },
+                child: PaymentMethodItem(
+                  isActive: activeIndex == index,
+                  image: paymentMethodsItems[index],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class PaymentMethodItem extends StatelessWidget {
+  const PaymentMethodItem({
+    super.key,
+    required this.isActive,
+    required this.image,
+  });
+
+  final bool isActive;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      width: 103.w,
+      height: 62.h,
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 3.w,
+            color: isActive ? ColorManager.kPrimaryColor : Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        shadows: [
+          BoxShadow(
+            color: isActive ? const Color(0xFF34A853) : Colors.white,
+            blurRadius: 4,
+            offset: const Offset(0, 0),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), color: Colors.white),
+        child: Center(
+          child: SvgPicture.asset(
+            image,
+            width: 60.w,
+          ),
+        ),
       ),
     );
   }
