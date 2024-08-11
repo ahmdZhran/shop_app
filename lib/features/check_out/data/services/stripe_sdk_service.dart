6232c35e77f7d1ea2db85e9c86ae80d22bc1be3d
Ahmed <ahmedmohameddx80@gmail.com>
2024-08-11 18:08:48 +0300

    create striepSdkService

---
+++ lib/features/check_out/data/services/stripe_sdk_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+class StripeSdkService {
+  Future<void> initPaymentSheet(
+      {required String paymentIntentClientSecret}) async {
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentClientSecret,
+        merchantDisplayName: 'Ahmed', // Name of the business owner
+      ),
+    );
+  }
+}
