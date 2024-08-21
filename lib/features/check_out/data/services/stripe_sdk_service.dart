432b42d30db0db66014b665189ac448e52bdeb3e
Ahmed <ahmedmohameddx80@gmail.com>
2024-08-21 19:11:24 +0300

    remove un important file and comments

--- lib/features/check_out/data/services/stripe_sdk_service.dart
+++ lib/features/check_out/data/services/stripe_sdk_service.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeSdkService {
@@ -15,21 +14,17 @@ class StripeSdkService {
   Future<bool> displayPaymentSheet() async {
     try {
       await Stripe.instance.presentPaymentSheet();
-      // Assuming payment is confirmed within presentPaymentSheet
       print("Payment completed successfully");
       return true;
     } on StripeException catch (e) {
       if (e.error.code == FailureCode.Canceled) {
-        // Handle the case where the user cancels the payment
         print('Payment canceled by user');
         return false;
       } else {
-        // Handle any other Stripe-specific errors
         print('Payment failed: ${e.error.localizedMessage}');
         return false;
       }
     } catch (error) {
-      // Handle any non-Stripe exceptions
       print('Error displaying payment sheet: $error');
       return false;
     }
