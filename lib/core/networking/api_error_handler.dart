1d50fa167493a97ef7b6cfadf1b537edab6d2085
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-03 09:22:36 +0300

    delete unused method , classes and variables we didn't use it

--- lib/core/networking/api_error_handler.dart
+++ lib/core/networking/api_error_handler.dart
@@ -18,26 +18,6 @@ enum DataSource {
   defaultError
 }
 
-class ResponseCode {
-  static const int success = 200; // success with data
-  static const int noContent = 201; // success with no data (no content)
-  static const int badRequest = 400; // failure, API rejected request
-  static const int unauthorized = 401; // failure, user is not authorized
-  static const int forbidden = 403; // failure, API rejected request
-  static const int internalServerError = 500; // failure, crash in server side
-  static const int notFound = 404; // failure, not found
-  static const int apiLogicError = 422; // API, LOGIC ERROR
-
-  // Local status codes
-  static const int connectTimeout = -1;
-  static const int cancel = -2;
-  static const int receiveTimeout = -3;
-  static const int sendTimeout = -4;
-  static const int cacheError = -5;
-  static const int noInternetConnection = -6;
-  static const int defaultError = -7;
-}
-
 class ResponseMessage {
   static const String noContent = ApiErrors.noContent;
   static const String badRequest = ApiErrors.badRequestError;
