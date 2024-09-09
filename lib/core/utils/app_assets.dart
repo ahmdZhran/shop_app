092b4b8b9b416fbe8cbe2ea0a9a65e5efebba0c4
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-09 17:10:31 +0300

    restore some assets from deleted assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -9,8 +9,6 @@ class AppAssets {
   static const String cartActive = "assets/images/cart_active.svg";
   static const String profileActive = "assets/images/user_active.svg";
   static const String profile = "assets/images/user.svg";
-  static const String category = "assets/images/category.svg";
-  static const String categoryActive = "assets/images/category_active.svg";
   static const String home = "assets/images/home.svg";
   static const String homeActive = "assets/images/home_active.svg";
 
